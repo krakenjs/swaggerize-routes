8dc3a0b5f2c2875ff591707290ec0103ac78136f
Trevor Livingston <trlivingston@paypal.com>
2014-08-26 13:06:07 -0500

    Added additional test coverage for validation

--- test/test-validation.js
+++ test/test-validation.js
@@ -104,4 +104,73 @@ test('validation', function (t) {
         });
     });
 
+    t.test('input pass (no model)', function (t) {
+        t.plan(1);
+
+        validation.input({
+            paramType: 'query',
+            name: 'id',
+            required: true
+        }, undefined)(1, function (error) {
+            t.ok(!error, 'no error.');
+        });
+    });
+
+    t.test('input fail (bad type)', function (t) {
+        t.plan(1);
+
+        inputvalid('hello', function (error) {
+            t.ok(error, 'error.');
+        });
+    });
+
+    t.test('output pass', function (t) {
+        t.plan(1);
+
+        outputvalid({
+            id: 1,
+            name: 'Test'
+        }, function (error) {
+            t.ok(!error, 'error.');
+        });
+    });
+
+    t.test('output fail (schema fail)', function (t) {
+        t.plan(3);
+
+        outputvalid({
+            id: 1,
+            name: 2
+        }, function (error) {
+            t.ok(error, 'error.');
+        });
+
+        outputvalid({
+            id: 'Test',
+            name: 1
+        }, function (error) {
+            t.ok(error, 'error.');
+        });
+
+        outputvalid('Test', function (error) {
+            t.ok(error, 'error.');
+        });
+    });
+
+    t.test('output fail (missing required)', function (t) {
+        t.plan(2);
+
+        outputvalid({
+            id: 1
+        }, function (error) {
+            t.ok(error, 'error.');
+        });
+
+        outputvalid({
+            name: 'Test'
+        }, function (error) {
+            t.ok(error, 'error.');
+        });
+    });
+
 });
